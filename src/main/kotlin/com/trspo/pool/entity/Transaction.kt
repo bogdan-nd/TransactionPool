24604183f813528a4fe651920940249ae2bbd2dd
bogdan-nd <bogdan6401@gmail.com>
2020-12-20 01:01:38 +0200

    changed transaction service

--- src/main/kotlin/com/trspo/pool/entity/Transaction.kt
+++ src/main/kotlin/com/trspo/pool/entity/Transaction.kt
@@ -35,15 +35,40 @@ data class Transaction(
         return String.format("Transaction:\nid -> %s\ndata -> %s",id,data);
     }
 
-    public fun fromTransactionMessage(response:TransactionMessage):Transaction{
-        val transactionId:UUID = UUID.fromString(response.id)
-        return Transaction(transactionId, response.data)
-    }
-
-    public fun toTransactionMessage():TransactionMessage{
+    fun toTransactionMessage():TransactionMessage{
         return TransactionMessage.newBuilder()
                 .setId(id.toString())
                 .setData(data)
                 .build()
+    }
+
+    companion object{
+        fun toTransactionBatchResponse(transactions: List<Transaction>):TransactionBatchResponse{
+            val transactionMessageList:MutableList<TransactionMessage> = ArrayList()
+
+            for(transaction in transactions){
+                val transactionMessage = transaction.toTransactionMessage()
+                transactionMessageList.add(transactionMessage)
+            }
+
+            return TransactionBatchResponse.newBuilder()
+                    .addAllTransactions(transactionMessageList)
+                    .build()
+        }
+
+        fun fromTransactionMessage(response: TransactionMessage): Transaction {
+            val transactionId: UUID = UUID.fromString(response.id)
+            return Transaction(transactionId, response.data)
+        }
+
+        fun fromTransactionBatch(response: TransactionBatchRequest):List<Transaction>{
+            val transactions:MutableList<Transaction> = ArrayList()
+
+            for(transactionMessage in response.transactionsList){
+                val newTransaction = fromTransactionMessage(transactionMessage)
+                transactions.add(newTransaction)
+            }
+            return transactions
+        }
     }
 }
--- src/main/kotlin/com/trspo/pool/repository/TransactionRepository.kt
+++ src/main/kotlin/com/trspo/pool/repository/TransactionRepository.kt
@@ -2,8 +2,12 @@ package com.trspo.pool.repository
 
 import com.trspo.pool.entity.Transaction
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface TransactionRepository: JpaRepository<Transaction,UUID> {
+
+    @Query("SELECT * FROM transaction GROUP BY id,RANDOM() LIMIT :amount",nativeQuery = true)
+    fun getTransactionsBatch(amount:Int):List<Transaction>
 
 }
